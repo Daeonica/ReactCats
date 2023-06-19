1bf59b3132085a84ced117340477f034d99fa8f1
daeonica <pauem13@gmail.com>
2023-06-19 17:51:42 +0200

    Aplicacion funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 //const que nos devuelve una imagen
 const CAT_ENDPOINT_IMAGE_URL = 'https://catass.com/cat/says/${firstWord}?size=50&color=read&son=true';
 
 //const que devuelve un random fact
-const CAT_ENDPOINT_RANDOM_FACT = 'https://atfact.ninja/fact';
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App() {
-    const [fact, setFact] = useState('lorem ipsum cat fact');
+    const [fact, setFact] = useState('');
+    const [imageUrl, setImageUrl] = useState();
 
     //para hacer el fetch se usa el useEffect porque queremos que el fetch se renderice cuando se monta el componente
     useEffect(() => {
         //hacemos el fetch
-        fetch(CAT_ENDPOINT_IMAGE_URL)
-        //devuelve la promesa y la respuesta
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+            //devuelve la promesa y la respuesta
             .then(res => res.json())
             //data es toda la informcion en formato json, y accedemos a .fact
             .then(data => {
-                const {fact} = data
+                const { fact } = data
                 setFact(fact)
 
-                const firstWord = fact.split(' ')[0] //nos separa una cadena de texto a partir del token que digamos, en este caso un espacio. Nos devuelve un array
-                console.log(firstWord);
+                const threeFirstWords = fact.split(' ', 3).join(' ') //nos separa una cadena de texto a partir del token que digamos, en este caso un espacio. Nos devuelve un array
+                console.log(threeFirstWords);
+
+                fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=read&json=true`)
+                    .then(res => res.json())
+                    .then(response => {
+                        const { url } = response
+                        setImageUrl(url)
+                    })
             })
     }, []) //  con el [] se renderiza la primera vez, sin el [] se renderiza siempre, si pusieramos [setFact] se renderizaria cuando modifiquemos el fact
+    
     return (
         <main>
-            <h1>App de gatitos</h1>  
-            {fact && <p>{fact}</p>} 
+            <h1>App de gatitos</h1>
+            {fact && <p>{fact}</p>}
+            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words of ${fact}`} />}
         </main>
     )
 }
